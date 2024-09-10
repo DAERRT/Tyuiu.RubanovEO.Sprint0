d26485573b516c4fe4012f0d9ab4427ce055ecf6
ЧЕЛОВЕК ЧЕЛОВЕК <olegoviz.2006@gmail.com>
2024-09-10 22:31:10 +0500

    Task1

---
+++ Tyuiu.RubanovEO.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.RubanovEO.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Egor";
+            int age = 18;
+            Console.WriteLine("Hello, my name is " + name + ", i'm " + age + " y.o!");
+            Console.ReadKey();
+        }
+    }
+}
